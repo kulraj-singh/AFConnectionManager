5926feef27522e2b4e41340f51b27dac4e5e007d
Kulraj Singh <koolraj003@gmail.com>
2016-02-18 11:54:50 +0530

    updated for afnetworking 3.0

--- AFConnectionManager.h
+++ AFConnectionManager.h
@@ -7,6 +7,8 @@
 
 //please go through the readme
 
+#define kBaseUrl @"http://www.projectsonseoxperts.net.au/instarideshare/live/v1/"
+
 #define kResponseObject @"response_object"
 #define kError @"error"
 #define kRequestType @"requestType"
@@ -19,10 +21,34 @@ typedef enum {
 }responseType;
 
 typedef enum {
-    TASK_GET = 200,
-    TASK_POST,
-    TASK_OTHER,
-    TASK_UPLOAD_PHOTO
+    TASK_LOGIN = 200,
+    TASK_SIGNUP,
+    TASK_RESET_PASSWORD,
+    TASK_UPDATE_USERNAME,
+    TASK_UPDATE_EMAIL,
+    TASK_UPDATE_PHONE,
+    TASK_UPLOAD_VIDEO,
+    TASK_UPLOAD_IMAGE,
+    TASK_GALLERY,
+    TASK_RECORD_VIEW,
+    TASK_RECORD_SCREENSHOT,
+    TASK_GET_USER_LIST,
+    TASK_ADD_FRIEND,
+    TASK_GET_FRIEND_LIST,
+    TASK_ACCEPT_FRIEND_REQUEST,
+    TASK_FORGOT_PASSWORD,
+    TASK_OFFER,
+    TASK_REQUEST,
+    TASK_OFFER_DETAILS,
+    TASK_REQUEST_DETAILS,
+    TASK_SAVE_OFFER,
+    TASK_SAVE_REQUEST,
+    TASK_INSTANT_REQUEST,
+    TASK_GET_ANIMALS,
+    TASK_VIEW_CONTACT_DETAIL,
+    TASK_BUY_CREDITS,
+    TASK_GET_PLAN,
+    TASK_INSTANT_OFFER,
 }taskType;
 
 typedef enum {
@@ -50,6 +76,13 @@ typedef enum {
 - (void)uploadPhoto:(UIImage*)image urlEndPoint:(NSString*)endPoint requestType:(taskType)requestType params:(NSMutableDictionary*)parameters imageKey:(NSString*)imageKey;
 
 - (void)uploadPhoto:(UIImage*)image urlEndPoint:(NSString*)endPoint requestType:(taskType)requestType params:(NSMutableDictionary*)parameters imageKey:(NSString*)imageKey responseFormat:(responseType)responseFormat;
+
+//video
+- (void)uploadVideoWithLocalUrl:(NSURL*)videoUrl urlEndPoint:(NSString*)endPoint requestType:(taskType)requestType params:(NSMutableDictionary*)postDict videoKey:(NSString*)videoKey imageKey:(NSString*)imageKey;
+
+//upload two photos
+//pass nil for second image and second image key if only one is there
+- (void)uploadPhoto:(UIImage *)image secondPhoto:(UIImage*)secondImage urlEndPoint:(NSString *)endPoint requestType:(taskType)requestType params:(NSMutableDictionary *)parameters imageKey:(NSString *)imageKey secondImageKey:(NSString*)secondImageKey;
 
 //call any get/post/put/patch/delete service
 - (void)callServiceWithRequestType:(taskType)requestType method:(serviceMethod)method params:(NSMutableDictionary*)params urlEndPoint:(NSString*)endPoint;
--- AFConnectionManager.m
+++ AFConnectionManager.m
@@ -5,9 +5,9 @@
 //  Created by Kulraj Singh on 16/06/15.
 //
 
-#define kBaseUrl @"http://www.raywenderlich.com/demos/weather_sample"
-
 #import "AFConnectionManager.h"
+#import "UIImage+ImageManipulations.h"
+#import "AFHTTPRequestOperation.h"
 
 @interface AFConnectionManager ()
 
@@ -29,17 +29,9 @@
 - (AFHTTPSessionManager*)sessionManagerForResponseFormat:(responseType)responseFormat requestType:(taskType)requestType
 {
     NSURL *baseURL = [NSURL URLWithString:kBaseUrl];
-    //change the base url on case basis. We can check the request type and give the base url
-    switch (requestType) {
-        case TASK_UPLOAD_PHOTO:
-            baseURL = [NSURL URLWithString:@"http://projectsonseoxperts.net.au/frequentflowers/live/public/api/"];
-            break;
-            
-        default:
-            break;
-    }
     
     AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:baseURL];
+    
     switch (responseFormat) {
         case RESPONSE_JSON:
             manager.responseSerializer = [AFJSONResponseSerializer serializer];
@@ -71,13 +63,14 @@
 {
     //any response format including plist
     _sessionManager = [self sessionManagerForResponseFormat:responseFormat requestType:requestType];
+    _sessionManager.responseSerializer.acceptableContentTypes = [_sessionManager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
     
     //change base url on case basis, if need be
     
     switch (method) {
         case METHOD_GET:
         {
-            [_sessionManager GET:endPoint parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
+            [_sessionManager GET:endPoint parameters:params progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
                 [self requestFinishedWithResponse:responseObject requestType:requestType];
             } failure:^(NSURLSessionDataTask *task, NSError *error) {
                 [self requestFailedWithError:error requestType:requestType];
@@ -87,7 +80,7 @@
             
         case METHOD_POST:
         {
-            [_sessionManager POST:endPoint parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
+            [_sessionManager POST:endPoint parameters:params progress:nil success:^(NSURLSessionDataTask *task, id responseObject) {
                 [self requestFinishedWithResponse:responseObject requestType:requestType];
             } failure:^(NSURLSessionDataTask *task, NSError *error) {
                 [self requestFailedWithError:error requestType:requestType];
@@ -130,6 +123,31 @@
     }
 }
 
+#pragma mark - video
+
+- (void)uploadVideoWithLocalUrl:(NSURL *)videoUrl urlEndPoint:(NSString *)endPoint requestType:(taskType)requestType params:(NSMutableDictionary *)postDict videoKey:(NSString *)videoKey imageKey:(NSString *)imageKey
+{
+    _sessionManager = [self sessionManagerForResponseFormat:RESPONSE_JSON requestType:requestType];
+    _sessionManager.responseSerializer.acceptableContentTypes = [_sessionManager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
+    
+    UIImage *thumbnail = [UIImage videoSnapshotFromUrl:videoUrl];
+    NSData *imgData = UIImagePNGRepresentation(thumbnail);
+    
+    [_sessionManager POST:endPoint parameters:postDict constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        //do not put image inside parameters dictionary as I did, but append it!
+        [formData appendPartWithFileData:imgData name:imageKey fileName:@"image.png" mimeType:@"image/png"];
+        
+        NSData *videoData = [NSData dataWithContentsOfURL:videoUrl];
+        [formData appendPartWithFileData:videoData name:@"video" fileName:@"video.mov" mimeType:@"video/quicktime"];
+    }
+      progress:nil
+      success:^(NSURLSessionDataTask *task, id responseObject) {
+        [self requestFinishedWithResponse:responseObject requestType:requestType];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        [self requestFailedWithError:error requestType:requestType];
+    }];
+}
+
 #pragma mark - photo
 
 - (void)uploadPhoto:(UIImage *)image urlEndPoint:(NSString *)endPoint requestType:(taskType)requestType params:(NSMutableDictionary *)parameters imageKey:(NSString *)imageKey
@@ -137,25 +155,77 @@
     [self uploadPhoto:image urlEndPoint:endPoint requestType:requestType params:parameters imageKey:imageKey responseFormat:RESPONSE_JSON];
 }
 
--(void)uploadPhoto:(UIImage*)image urlEndPoint:(NSString*)endPoint requestType:(taskType)requestType params:(NSMutableDictionary*)parameters imageKey:(NSString*)imageKey responseFormat:(responseType)responseFormat
+- (void)uploadPhoto:(UIImage*)image urlEndPoint:(NSString*)endPoint requestType:(taskType)requestType params:(NSMutableDictionary*)parameters imageKey:(NSString*)imageKey responseFormat:(responseType)responseFormat
 {
     _sessionManager = [self sessionManagerForResponseFormat:responseFormat requestType:requestType];
     _sessionManager.responseSerializer.acceptableContentTypes = [_sessionManager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
     
-    NSData *imageData = UIImageJPEGRepresentation(image, 1);
-    NSInteger imageSize = imageData.length;
+    image = [image fixrotation];
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+//    NSInteger imageSize = imageData.length;
+//    
+//    //limit image size to 100 kb
+//    float maxSize = 100000; //100 kb
+//    if (imageSize > maxSize) {
+//        float compressionFactor = maxSize/imageSize;
+//        imageData = (UIImageJPEGRepresentation(image, compressionFactor));
+//    }  
+    
+    [_sessionManager POST:endPoint parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        //do not put image inside parameters dictionary as I did, but append it!
+        [formData appendPartWithFileData:imageData name:imageKey fileName:@"image.png" mimeType:@"image/png"];
+    } progress:nil
+      success:^(NSURLSessionDataTask *task, id responseObject) {
+        [self requestFinishedWithResponse:responseObject requestType:requestType];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        [self requestFailedWithError:error requestType:requestType];
+    }];
+}
+
+- (void)uploadPhoto:(UIImage*)image secondPhoto:(UIImage *)secondImage urlEndPoint:(NSString *)endPoint requestType:(taskType)requestType params:(NSMutableDictionary *)parameters imageKey:(NSString *)imageKey secondImageKey:(NSString *)secondImageKey
+{
+    _sessionManager = [self sessionManagerForResponseFormat:RESPONSE_JSON requestType:requestType];
+    _sessionManager.responseSerializer.acceptableContentTypes = [_sessionManager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
     
     //limit image size to 100 kb
     float maxSize = 100000; //100 kb
-    if (imageSize > maxSize) {
-        float compressionFactor = maxSize/imageSize;
-        imageData = (UIImageJPEGRepresentation(image, compressionFactor));
+    
+    NSData *imageData;
+    if (image) {
+        imageData = UIImageJPEGRepresentation(image, 1.0);
+        NSInteger imageSize = imageData.length;
+        
+        if (imageSize > maxSize) {
+            float compressionFactor = maxSize/imageSize;
+            imageData = UIImageJPEGRepresentation(image, compressionFactor);
+        }
+    }
+    
+    NSData *secondImageData;
+    if (secondImage) {
+        
+        secondImageData = UIImageJPEGRepresentation(secondImage, 1.0);
+        NSInteger secondImageSize = secondImageData.length;
+        
+        if (secondImageSize > maxSize) {
+            float compressionFactor = maxSize/secondImageSize;
+            secondImageData = UIImageJPEGRepresentation(secondImage, compressionFactor);
+        }
     }
     
     [_sessionManager POST:endPoint parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
         //do not put image inside parameters dictionary as I did, but append it!
-        [formData appendPartWithFileData:imageData name:imageKey fileName:@"image" mimeType:@"image/jpeg"];
-    } success:^(NSURLSessionDataTask *task, id responseObject) {
+        if (image) {
+            [formData appendPartWithFileData:imageData name:imageKey fileName:@"frontImage.jpg" mimeType:@"image/jpeg"];
+        }
+        
+        if (secondImage) {
+            [formData appendPartWithFileData:secondImageData name:secondImageKey fileName:@"backImage.jpg" mimeType:@"image/jpeg"];
+        }
+    }
+      progress:nil
+      success:^(NSURLSessionDataTask *task, id responseObject) {
         [self requestFinishedWithResponse:responseObject requestType:requestType];
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         [self requestFailedWithError:error requestType:requestType];
@@ -171,21 +241,12 @@
 
 - (void)getContentOfUrl:(NSString *)urlString requestType:(taskType)requestType responseFormat:(responseType)responseFormat
 {
-    NSURL *url = [NSURL URLWithString:urlString];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-    operation.responseSerializer = [AFImageResponseSerializer serializer];
-    
-    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET:urlString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
         [self requestFinishedWithResponse:responseObject requestType:requestType];
-        
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
         [self requestFailedWithError:error requestType:requestType];
     }];
-    [operation start];
 }
 
 #pragma mark - cancel request
